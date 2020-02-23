91d953d73555d26884f55de1323f6dc5277cc2ff
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-02-23 22:49:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,14 +3,8 @@ array.index(value_to_find)
 end
 
 def find_max_value(array)
-i = 0
-max = 0
-while i < array.length do
-  if array[i] > max
-    max = array[i]
-end
-i += 1
-return max
+array.sort!
+return array[-1]
 end
 
 def find_min_value(array)
