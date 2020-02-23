7c2a8198529eb339d39dbe979b88797ff5187d1a
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-02-23 22:48:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,16 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+i = 0
+max = 0
+while i < array.length do
+  if array[i] > max
+    max = array[i]
+end
+i += 1
+return max
 end
 
 def find_min_value(array)
